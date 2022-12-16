45c38d2babaca1a20a31d8ceedd79783fca83599
DetariLeon <detari.leon.2020i@bankitatabanya.hu>
2022-12-16 13:07:37 +0100

    12.16

--- derekszog12.14/app.js
+++ derekszog12.14/app.js
@@ -14,21 +14,29 @@ function Derekszog() {
 function MeterAtvaltas() {
     let m = parseInt(document.getElementById("szam").value)
 
-    document.write(m + " méter egyenlő:" + '<br>')
-    document.write(m * 3.280839895 + "ft" + '<br>')
-    document.write(m * 0.00062137 + " mile" + '<br>')
-    document.write(m / 1000 + " km" + '<br>')
-    document.write(m * 1.0936133 + " yard" + '<br>')
-    document.write(m * 39.3700787 + " inch" + '<br>')
+    document.getElementById("meterAt").innerHTML=(m + " méter egyenlő:" + '<br>' + 
+    m * 3.280839895 + "ft" + '<br>' + 
+    m * 3.280839895 + "ft" + '<br>' + 
+    m * 0.00062137 + " mile" + '<br>' + 
+    m / 1000 + " km" + '<br>' + 
+    m * 1.0936133 + " yard" + '<br>' + 
+    m * 39.3700787 + " inch" + '<br>')
+    
 
 }
 function Masodperc() {
-    mp = parseInt(document.getElementById("masodperc").value)
-    document.write (hours = Math.floor(mp / 3600));
-    document.write (minutes = Math.floor((mp % 3600) / 60));
-    document.write (secs = mp % 60);
-  
-   
-    document.write ( Math.floor(hours) + " hours, " + minutes + " minutes, " + secs + " seconds");
-  }
-  
+    let fMp = parseInt(document.getElementById("masodperc").value)
+
+    let sec = fMp
+    let hour = Math.floor(sec / 3600)
+    sec = sec % 3600
+    let min = Math.floor(sec / 60)
+    sec = sec % 60
+    document.getElementById("mp").innerHTML = "" + hour + "-" + min + "-" + sec
+}
+function hatszogTerulet() {
+    let hatSz = parseInt(document.getElementById("hatszogT").value)
+    document.getElementById("hSz").innerHTML = "Hatszög területe: " + ((3 * Math.sqrt(3)) / 2) * Math.pow(hatSz, 2)
+}
+
+
