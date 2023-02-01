d15ac1161ec254872865c266e586745427654a49
DetariLeon <detari.leon.2020i@bankitatabanya.hu>
2023-02-01 13:34:39 +0100

    13.34

---
+++ Javascript0201/jsesemyenykezelo.js
@@ -0,0 +1,20 @@
+function vizsgal(){
+    var szoveg =  document.getElementById("name").value
+    document.getElementById("ideir").innerHTML = szoveg
+    
+
+     
+ }
+ function validateEmail() {
+    var email = document.getElementById("email").value
+    
+    if (email.length >= 5 && email.includes('@') && email.includes('.')) {
+        document.getElementById("EmailIfValid").innerHTML = "Jó"
+    } else {
+        document.getElementById("EmailIfValid").innerHTML = "Nem jo"
+    }
+
+   
+    var regex = /^[^@]+@[^.]+\.[a-z]+$/i;
+    console.log(regex.test(email) && email.length >= 5)
+}
