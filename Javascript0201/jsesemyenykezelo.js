e17794619f8fe67aaf2e17477c73088554efe380
DetariLeon <detari.leon.2020i@bankitatabanya.hu>
2023-02-07 13:16:38 +0100

    02.07

--- Javascript0201/jsesemyenykezelo.js
+++ Javascript0201/jsesemyenykezelo.js
@@ -1,20 +1,24 @@
-function vizsgal(){
-    var szoveg =  document.getElementById("name").value
+function vizsgal() {
+    var szoveg = document.getElementById("name").value
     document.getElementById("ideir").innerHTML = szoveg
-    
 
-     
- }
- function validateEmail() {
+
+
+}
+function validateEmail() {
     var email = document.getElementById("email").value
-    
+
     if (email.length >= 5 && email.includes('@') && email.includes('.')) {
         document.getElementById("EmailIfValid").innerHTML = "Jó"
     } else {
         document.getElementById("EmailIfValid").innerHTML = "Nem jo"
     }
 
-   
+
     var regex = /^[^@]+@[^.]+\.[a-z]+$/i;
     console.log(regex.test(email) && email.length >= 5)
+}
+function felhasznalonev() {
+    var nev = document.getElementById("Fnev").value
+    document.getElementById("felhasznalonev").innerHTML = nev
 }
